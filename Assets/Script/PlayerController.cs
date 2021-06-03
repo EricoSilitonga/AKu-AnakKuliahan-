44541c6444431bea6b832a7c06d45c035388ec38
EricoSilitonga <ericosilitonga@yahoo.com>
2021-06-03 15:40:53 +0700

    Project Uploaded

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Rigidbody2D playerRB;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * _speed * Time.deltaTime;
+        playerAnim.SetFloat("MoveX", playerRB.velocity.x);
+        playerAnim.SetFloat("MoveY", playerRB.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            playerAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            playerAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
